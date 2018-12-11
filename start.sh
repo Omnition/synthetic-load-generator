217c16d1b0d989e71e7e0977013d67ddf6a995c2
Mayank Agarwal <9286568+maynk@users.noreply.github.com>
2018-12-10 23:51:30 -0800

    Fix bug in start.sh syntax (#4)

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-JAEGER_COLLECTOR_URL?=http://jaeger-collector:14268
-TOPOLOGY_FILE?=topologies/hipster-shop.json
+if [ -z "${JAEGER_COLLECTOR_URL}" ]; then JAEGER_COLLECTOR_URL=http://jaeger-collector:14268; fi
+if [ -z "${TOPOLOGY_FILE}" ]; then TOPOLOGY_FILE=topologies/hipster-shop.json; fi
 
 java -jar synthetic-load-generator.jar \
     --paramsFile ${TOPOLOGY_FILE} \
