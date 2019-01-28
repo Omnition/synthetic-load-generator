635e7d19562d9aaaf5de8d9794a509f5b36d1ce2
Paulo Janotti <paulo@omnition.io>
2019-01-28 14:58:09 -0800

    Create a topology file that generates 100K spans per sec
    Oportunistically change the start.sh to not always add JAEGER.

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 if [ -z "${JAEGER_COLLECTOR_URL}" ]; then JAEGER_COLLECTOR_URL=http://jaeger-collector:14268; fi
-if [ -z "${TOPOLOGY_FILE}" ]; then TOPOLOGY_FILE=topologies/hipster-shop.json; fi
-
-PARAMS="--jaegerCollectorUrl ${JAEGER_COLLECTOR_URL}"
+if [ -z "${TOPOLOGY_FILE}" ]; then TOPOLOGY_FILE=./topologies/hipster-shop.json; fi
 
 if [ ! -z "${ZIPKINV1_COLLECTOR_URL}" ]; then
     PARAMS="$PARAMS --zipkinV1CollectorUrl ${ZIPKINV1_COLLECTOR_URL}"
@@ -13,7 +11,11 @@ if [ ! -z "${ZIPKINV2_COLLECTOR_URL}" ]; then
     PARAMS="$PARAMS --zipkinV2CollectorUrl ${ZIPKINV2_COLLECTOR_URL}"
 fi
 
-echo "using params: " $PARAMS
+if [ -z "${PARAMS}" ]; then
+    PARAMS="--jaegerCollectorUrl ${JAEGER_COLLECTOR_URL}"
+fi
+
+echo "using params: " ${PARAMS}
 
 java -jar synthetic-load-generator.jar \
     --paramsFile ${TOPOLOGY_FILE} \
