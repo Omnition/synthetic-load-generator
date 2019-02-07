5862909d769c52c86c2486e3bed385e99306422e
Steven Karis <skaris@omnition.io>
2019-02-06 17:57:21 -0800

    Allow jar to take multiple topology files
    Allowing for multiple topology files allows us to create very
    sophisticated test scenarios.
    
    Testing Done: locally

--- src/main/java/io/omnition/loadgenerator/model/topology/TagGenerator.java
+++ src/main/java/io/omnition/loadgenerator/model/topology/TagGenerator.java
@@ -11,16 +11,23 @@ public class TagGenerator {
     private Random rand = new Random();
     private TagNameGenerator tagGen = new TagNameGenerator();
 
+    public String tagName;
     public int valLength = 10;
     public int numTags = 0;
     public int numVals = 0;
 
     public Map<String, Object> generateTags() {
         Map<String, Object> retVal = new HashMap<>();
+        if (numTags != 0 && tagName != null) {
+            throw new IllegalArgumentException("numTags and tagName cannot both be set");
+        }
         for (int genIndex = 0; genIndex < numTags; genIndex++) {
             String val;
             val = RandomStringUtils.random(valLength, 0, 0, true, true, null, new Random(rand.nextInt(numVals)));
             retVal.put(tagGen.getForIndex(genIndex), val);
+        }
+        if (tagName != null) {
+            retVal.put(tagName, RandomStringUtils.random(valLength, 0, 0, true, true, null, new Random(rand.nextInt(numVals))));
         }
 
         return retVal;
